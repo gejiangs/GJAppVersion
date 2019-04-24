4396601c05a7a1647d0fecb57ed3e1a52e537fb8
gejiangs <664553782@qq.com>
2019-04-24 14:52:41 +0800

    Initial Commit

---
+++ GJAppVersion/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GJAppVersion
+//
+//  Created by gejiangs on 2019/4/24.
+//  Copyright © 2019 gejiangs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GJAppVersion/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GJAppVersion
+//
+//  Created by gejiangs on 2019/4/24.
+//  Copyright © 2019 gejiangs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
